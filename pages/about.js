aca97b1de160ab40c3ff7bb728171431c0d8149a
KingAlex1997 <alex97l@hotmail.com>
2022-05-04 00:32:48 -0300

    first apk with next js - added about page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <Link href="/about">About</Link>
+          Ir a <Link href="/">Home</Link>
         </h1>
 
         <p className={styles.description}>
