9c8bb1b74f73295832a9377a03dc3a94050e327b
KingAlex1997 <alex97l@hotmail.com>
2022-05-06 14:53:17 -0300

    map de navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,31 @@ import styles from "./Navbar.module.css";
 import React from "react";
 import { ActiveLink } from "./ActiveLink";
 
+const menuItems = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "About",
+    href: "/about",
+  },
+  {
+    text: "Contact",
+    href: "/contact",
+  },
+  {
+    text: "Pricing",
+    href: "/pricing",
+  },
+];
+
 export const Navbar = () => {
   return (
     <nav className={styles["nav-menu"]}>
-      <ActiveLink href="/" text="Home"></ActiveLink>
-      <ActiveLink href="/about" text="About"></ActiveLink>
-      <ActiveLink href="/contact" text="Contact"></ActiveLink>
+      {menuItems.map((item) => (
+        <ActiveLink key={item.text} href={item.href} text={item.text} />
+      ))}
     </nav>
   );
 };
--- pages/pricing.js
+++ pages/pricing.js
@@ -1,9 +1,10 @@
+import React from "react";
 import { MainLayout } from "../components/layouts/MainLayout";
 
-export default function Contact() {
+export default function Pricing() {
   return (
     <MainLayout>
-      <h1 className="title">Welcome Contact</h1>
+      <h1 className="title">Welcome Pricing</h1>
 
       <p className="description">
         Get started by editing <code className="code">pages/index.js</code>
